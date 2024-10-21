0f92f4160bffeb7a6055a3ff6183942176fb9da2
JosephChoKingwangjjang <josephcho0934@gmail.com>
2024-10-21 12:23:52 +0900

    깃헙

---
+++ index.js
@@ -0,0 +1,21 @@
+//alert("안녕하십니까")
+//confirm("정말 이 페이지로 들어가시겠습니까?")
+//prompt("느그 이름이 뭐노?")
+//prompt("느그 이름이 뭐노?", "느그 조상님이다")
+//document.write("<h1>왜 이렇게 늦었느냐?<h1>")
+//console.log("후후후후훗")
+
+var currentYear = 2024;
+var birthYear;
+//var age;
+console.log(currentYear);
+console.log(birthYear)
+
+//var name;
+//let age;
+//const email = "아 난 왜 네이버 계정 못 만드냐고.com"
+
+birthYear = prompt("태어난 연도를 입력하세요 (YYYY)", "")
+age = currentYear - birthYear + 1;
+document.write(currentYear + "년 현재<br>");
+document.write(birthYear + "년에 태어난 사람의 나이는" + age + "세 입니다");
