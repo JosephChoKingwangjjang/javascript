07832ccf5c26fcdbf6657de87ae0e9206f02bc6b
JosephChoKingwangjjang <josephcho0934@gmail.com>
2024-11-04 10:20:04 +0900

    yeah

--- hometraining.js
+++ hometraining.js
@@ -2,8 +2,12 @@ var passWord
 var howMuch = 0;
 
 do {
-    passWord = prompt("암호를 입력하세요")
+    passWord = prompt("숫자를 입력하세요")
     if (passWord !== null) {
+         if (isNaN(passWord)) { // 입력값이 숫자가 아닌 경우 
+            alert("숫자만 인식할 수 있습니다");} 
+        else {
+        passWord = Number(passWord);
         if (passWord < 20090304) {
             alert("작습니다")
             howMuch++;
@@ -13,11 +17,10 @@ do {
         } else if (passWord = 20090304) {
             break;
         }
-    }
+    }}
 } while(passWord !== 20090304)
 
     if (passWord === 20090304) {
         alert("맞췄습니다")
-        howMuch++;
-    }
+        howMuch++; }
     document.write(howMuch + "번 시도하셨습니다.")
